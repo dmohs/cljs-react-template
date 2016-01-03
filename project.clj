d21c600ea3dc1c85d480508e35600a3651490c6d
David Mohs <dgit@mohsinc.com>
2016-01-03 15:12:25 -0500

    Add GitHub URL

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject dmohs.cljs-react/lein-template "0.0.1"
+  :url "https://github.com/dmohs/cljs-react-template"
   :description "ClojureScript + dmohs/react project template."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
